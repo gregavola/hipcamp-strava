c1931a2c428f3d9c0159e4a907acaa14855f3f78
Greg Avola <greg@untappd.com>
2022-03-15 09:41:54 -0400

    moving to Google Maps

--- utils/getStaticMapUrl.ts
+++ utils/getStaticMapUrl.ts
@@ -8,31 +8,7 @@ const encodeGeoJsonOverlay = (geojson: any) => {
 };
 
 export function getStaticMapUrl(summary_polyline?: string) {
-  const lineFeature = polyline.toGeoJSON(summary_polyline);
+  const staticGoogleImageUrl = `https://maps.googleapis.com/maps/api/staticmap?scale=2&size=750x750&maptype=roadmap&path=enc:${summary_polyline}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
 
-  const actualGeoJSON = {
-    type: "Feature",
-    geometry: lineFeature,
-    properties: { stroke: "#fc4c02" },
-  };
-
-  const tolerance =
-    actualGeoJSON.geometry.coordinates.length >= 500
-      ? 0.01
-      : actualGeoJSON.geometry.coordinates.length >= 400
-      ? 0.001
-      : 0.0001;
-
-  const lineFeatureSimple = simplify(actualGeoJSON, tolerance);
-
-  let width = 375;
-  let height = 375;
-  let baseMapStyle = "streets-v11";
-
-  const encodedOverlays = encodeGeoJsonOverlay(lineFeatureSimple);
-  const staticImageUrl = `https://api.mapbox.com/styles/v1/mapbox/${baseMapStyle}/static/${encodedOverlays}/auto/${width}x${height}@2x?access_token=${process.env.MAPBOX_API_KEY}&attribution=false&logo=false`;
-
-  console.log(staticImageUrl);
-
-  return staticImageUrl;
+  return staticGoogleImageUrl;
 }
